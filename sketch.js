32fecc77baeb2e1003ebf161a2750a58ba550bae
Tom White <tom@sixdozen.com>
2023-09-25 22:39:06 +1300

    Example of layers

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
+let curLayer = 0;
 
 // change these three lines as appropiate
 let sourceFile = "input_1.jpg";
@@ -25,43 +26,60 @@ function setup () {
 }
 
 function draw () {
-  let num_lines_to_draw = 40;
-  // get one scanline
-  for(let j=renderCounter; j<renderCounter+num_lines_to_draw && j<1080; j++) {
-    for(let i=0; i<1920; i++) {
-      colorMode(RGB);
-      let pix = sourceImg.get(i, j);
-      // create a color from the values (always RGB)
-      let col = color(pix);
-      let mask = maskImg.get(i, j);
+  if (curLayer == 0) {
+    let num_lines_to_draw = 40;
+    // get one scanline
+    for(let j=renderCounter; j<renderCounter+num_lines_to_draw && j<1080; j++) {
+      for(let i=0; i<1920; i++) {
+        colorMode(RGB);
+        let pix = sourceImg.get(i, j);
+        // create a color from the values (always RGB)
+        let col = color(pix);
+        let mask = maskImg.get(i, j);
 
-      colorMode(HSB, 360, 100, 100);
-      // draw a "dimmed" version in gray
-      let h = hue(col);
-      let s = saturation(col);
-      let b = brightness(col);
+        colorMode(HSB, 360, 100, 100);
+        // draw a "dimmed" version in gray
+        let h = hue(col);
+        let s = saturation(col);
+        let b = brightness(col);
 
-      if(mask[0] > 128) {
-        // draw the full pixels
-        // let new_sat = map(s, 0, 100, 50, 100);
-        let new_brt = map(b, 0, 100, 50, 100);
-        // let new_hue = map(h, 0, 360, 180, 540);
-        let new_col = color(0, s, new_brt);
-        set(i, j, new_col);
-      }
-      else {
-        // let new_brt = map(b, 0, 100, 20, 40);
-        let new_brt = map(b, 0, 100, 100, 0);
+        let new_brt = map(b, 0, 100, 30, 50);
         let new_col = color(h, 0, new_brt);
-        // let new_col = color(h, s, b);
         set(i, j, new_col);
       }
     }
+    renderCounter = renderCounter + num_lines_to_draw;
+    updatePixels();
   }
-  renderCounter = renderCounter + num_lines_to_draw;
-  updatePixels();
+  else {
+    rectMode(CORNERS);
+    for(let i=0; i<100; i++) {
+      let x1 = random(0, width);
+      let y1 = random(0, height);
+      let x2 = x1 + random(-10, 10);
+      let y2 = y1 + random(-10, 10);
+      colorMode(RGB);
+      let pix = sourceImg.get(x1, y1);
+      let mask = maskImg.get(x1, y1);
+      let col = color(pix);
+      stroke(col);
+      fill(col);
+      if(mask[1] < 128) {
+        line(x1, y1, x2, y2);
+      }
+      else {
+        rect(x1, y1, x2, y2);
+      }
+    }
+    renderCounter = renderCounter + 1;
+    // set(i, j, new_col);
+  }
   // print(renderCounter);
-  if(renderCounter > 1080) {
+  if(curLayer == 0 && renderCounter > 1080) {
+    curLayer = 1;
+    renderCounter = 0;
+  }
+  else if(curLayer == 1 && renderCounter > 500) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
