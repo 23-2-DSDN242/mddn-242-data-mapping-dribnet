9cf64f5c92846303b5b000cc654f7a2f742d80bb
Tom White <tom@sixdozen.com>
2023-10-02 23:25:20 +1300

    Focus blur, testing a corner

--- sketch.js
+++ sketch.js
@@ -1,7 +1,5 @@
 let sourceImg=null;
 let maskImg=null;
-let renderCounter=0;
-let curLayer = 0;
 
 // change these three lines as appropiate
 let sourceFile = "input_1.jpg";
@@ -25,82 +23,47 @@ function setup () {
   colorMode(HSB);
 }
 
+let X_STOP = 640;
+let Y_STOP = 480;
+let OFFSET = 6;
+
+let renderCounter=5;
 function draw () {
-  if (curLayer == 0) {
-    let num_lines_to_draw = 40;
-    // get one scanline
-    for(let j=renderCounter; j<renderCounter+num_lines_to_draw && j<1080; j++) {
-      for(let i=0; i<1920; i++) {
-        colorMode(RGB);
-        let pix = sourceImg.get(i, j);
-        // create a color from the values (always RGB)
-        let col = color(pix);
-        let mask = maskImg.get(i, j);
-
-        colorMode(HSB, 360, 100, 100);
-        // draw a "dimmed" version in gray
-        let h = hue(col);
-        let s = saturation(col);
-        let b = brightness(col);
-
-        let new_brt = map(b, 0, 100, 30, 50);
-        let new_col = color(h, 0, new_brt);
-        set(i, j, new_col);
-      }
-    }
-    renderCounter = renderCounter + num_lines_to_draw;
-    updatePixels();
-  }
-  else if (curLayer == 1) {
-    for(let i=0; i<500; i++) {
-      let x1 = random(0, width);
-      let y1 = random(0, height);
-      let mask = maskImg.get(x1, y1);
-      if(mask[1] > 128) {
-        let x2 = x1 + random(-20, 20);
-        let y2 = y1 + random(-20, 20);
-        colorMode(RGB);
-        stroke(255, 255, 0);
-        line(x1, y1, x2, y2);
-      }
-    }
-    renderCounter = renderCounter + 1;
-  }
-  else {
-    rectMode(CORNERS);
-    for(let i=0; i<100; i++) {
-      let x1 = random(0, width);
-      let y1 = random(0, height);
-      let x2 = x1 + random(-10, 10);
-      let y2 = y1 + random(-10, 10);
+  let num_lines_to_draw = 40;
+  // get one scanline
+  for(let j=renderCounter; j<renderCounter+num_lines_to_draw && j<1080; j++) {
+    for(let i=5; i<X_STOP; i++) {
       colorMode(RGB);
-      let pix = sourceImg.get(x1, y1);
-      let mask = maskImg.get(x1, y1);
-      let col = color(pix);
-      stroke(col);
-      fill(col);
-      if(mask[1] < 128) {
-        line(x1, y1, x2, y2);
+      let pix = [0, 0, 0, 255];
+      let mask = maskImg.get(i, j);
+      if (mask[1] > 128) {
+        pix = sourceImg.get(i, j);
       }
       else {
-        rect(x1, y1, x2, y2);
+        let sum_rgb = [0, 0, 0]
+        let num_cells = 0;
+        for(let wx=-OFFSET;wx<OFFSET;wx++){
+          for (let wy=-OFFSET;wy<OFFSET;wy++) {
+            let pix = sourceImg.get(i+wx, j+wy);
+            for(let c=0; c<3; c++) {
+              sum_rgb[c] += pix[c];
+            }
+            num_cells += 1;
+          }
+        }
+        for(let c=0; c<3; c++) {
+          pix[c] = int(sum_rgb[c] / num_cells);
+        }        
       }
+
+      set(i, j, pix);
     }
-    renderCounter = renderCounter + 1;
-    // set(i, j, new_col);
   }
+  renderCounter = renderCounter + num_lines_to_draw;
+  updatePixels();
+
   // print(renderCounter);
-  if(curLayer == 0 && renderCounter > 1080) {
-    curLayer = 1;
-    renderCounter = 0;
-    print("Switching to curLayer 1");
-  }
-  if(curLayer == 1 && renderCounter > 500) {
-    curLayer = 2;
-    renderCounter = 0;
-    print("Switching to curLayer 2");
-  }
-  else if(curLayer == 2 && renderCounter > 1500) {
+  if(renderCounter > Y_STOP) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
