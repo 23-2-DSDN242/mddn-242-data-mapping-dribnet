79f02122f235bed44835c835343b9844dc906b10
Tom White <tom@sixdozen.com>
2023-10-04 20:36:53 +1300

    mask scan example with bullseye

--- sketch.js
+++ sketch.js
@@ -2,9 +2,11 @@ let sourceImg=null;
 let maskImg=null;
 
 // change these three lines as appropiate
-let sourceFile = "input_1.jpg";
-let maskFile   = "mask_1.png";
-let outputFile = "output_1.png";
+let sourceFile = "input_new.jpg";
+let maskFile   = "mask_new.png";
+let outputFile = "output_4.png";
+let maskCenter = null;
+let maskCenterSize = null;
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -21,13 +23,64 @@ function setup () {
   sourceImg.loadPixels();
   maskImg.loadPixels();
   colorMode(HSB);
+
+  maskCenterSearch(20);
 }
 
-let X_STOP = 640;
-let Y_STOP = 480;
-// let X_STOP = 1920;
-// let Y_STOP = 1080;
+// let X_STOP = 640;
+// let Y_STOP = 480;
+let X_STOP = 1920;
+let Y_STOP = 1080;
 let OFFSET = 20;
+
+function maskCenterSearch(min_width) {
+    let max_up_down = 0;
+    let max_left_right = 0;
+    let max_x_index = 0;
+    let max_y_index = 0;
+
+    // first scan all rows top to bottom
+    print("Scanning mask top to bottom...")
+    for(let j=0; j<Y_STOP; j++) {
+      // look across this row left to right and count
+      let mask_count = 0;
+      for(let i=0; i<X_STOP; i++) {
+        let mask = maskImg.get(i, j);
+        if (mask[1] > 128) {
+          mask_count = mask_count + 1;
+        }
+      }
+      // check if that row sets a new record
+      if (mask_count > max_left_right) {
+        max_left_right = mask_count;
+        max_y_index = j;
+      }
+    }
+
+    // now scan once left to right as well
+    print("Scanning mask left to right...")
+    for(let i=0; i<X_STOP; i++) {
+      // look across this column up to down and count
+      let mask_count = 0;
+      for(let j=0; j<Y_STOP; j++) {
+        let mask = maskImg.get(i, j);
+        if (mask[1] > 128) {
+          mask_count = mask_count + 1;
+        }
+      }
+      // check if that row sets a new record
+      if (mask_count > max_up_down) {
+        max_up_down = mask_count;
+        max_x_index = i;
+      }
+    }
+
+    print("Scanning mask done!")
+    if (max_left_right > min_width && max_up_down > min_width) {
+      maskCenter = [max_x_index, max_y_index];
+      maskCenterSize = [max_left_right, max_up_down];
+    }
+}
 
 let renderCounter=0;
 function draw () {
@@ -42,14 +95,12 @@ function draw () {
         pix = sourceImg.get(i, j);
       }
       else {
-        let wave = sin(j*8);
-        let slip = map(wave, -1, 1, -OFFSET, OFFSET);
-        pix = sourceImg.get(i+slip, j);
-
-        // let brt = map(wave, -1, 1, 0, 255);
-        // for(let c=0; c<3; c++) {
-        //   pix[c] = brt;
-        // }
+        if(j%2 == 0) {
+          pix = [255, 255, 0, 255]
+        }
+        else {
+          pix = sourceImg.get(i, j);          
+        }
       }
 
       set(i, j, pix);
@@ -57,6 +108,19 @@ function draw () {
   }
   renderCounter = renderCounter + num_lines_to_draw;
   updatePixels();
+
+  if (maskCenter !== null) {
+    strokeWeight(5);
+    fill(0, 255, 0);
+    stroke(255, 0, 0);
+    ellipse(maskCenter[0], maskCenter[1], 100);
+    line(maskCenter[0]-200, maskCenter[1], maskCenter[0]+200, maskCenter[1]);
+    line(maskCenter[0], maskCenter[1]-200, maskCenter[0], maskCenter[1]+200);
+    noFill();
+    let mcw = maskCenterSize[0];
+    let mch = maskCenterSize[1];
+    rect(maskCenter[0]-mcw/2, maskCenter[1]-mch/2, mcw, mch);
+  }
 
   // print(renderCounter);
   if(renderCounter > Y_STOP) {
