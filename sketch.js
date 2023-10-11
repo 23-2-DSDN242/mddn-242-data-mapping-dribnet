f7300206957e3b7a5fb93f5d10c2cef584ce7eb0
tuakana-vuw <71419542+tuakana-vuw@users.noreply.github.com>
2023-10-12 10:40:30 +1300

    moving image example

--- sketch.js
+++ sketch.js
@@ -1,16 +1,19 @@
 let sourceImg=null;
 let maskImg=null;
-let textImg = null;
+let texImg=null;
 
 // change these three lines as appropiate
-let sourceFile = "input_new3.jpg";
-let maskFile   = "mask_new3.png";
-let outputFile = "output_new3.png";
+let sourceFile = "input_n.jpg";
+let maskFile   = "mask_n.png";
+let outputFile = "output_n.png";
+let textureFile = "texture_car.png"
+let maskCenter = null;
+let maskCenterSize = null;
 
 function preload() {
   sourceImg = loadImage(sourceFile);
   maskImg = loadImage(maskFile);
-  textImg = loadImage("texture2.png");
+  texImg = loadImage(textureFile);
 }
 
 function setup () {
@@ -22,44 +25,133 @@ function setup () {
   background(0, 0, 128);
   sourceImg.loadPixels();
   maskImg.loadPixels();
-  textImg.loadPixels();
+  texImg.loadPixels();
+  colorMode(HSB);
+
+  maskCenterSearch2(20);
 }
 
+// let X_STOP = 640;
+// let Y_STOP = 480;
 let X_STOP = 1920;
 let Y_STOP = 1080;
+let OFFSET = 20;
+
+function maskCenterSearch(min_width) {
+    let max_up_down = 0;
+    let max_left_right = 0;
+    let max_x_index = 0;
+    let max_y_index = 0;
+
+    // first scan all rows top to bottom
+    print("Scanning mask top to bottom...")
+    for(let j=0; j<Y_STOP; j++) {
+      // look across this row left to right and count
+      let mask_count = 0;
+      for(let i=0; i<X_STOP; i++) {
+        let mask = maskImg.get(i, j);
+        if (mask[1] > 128) {
+          mask_count = mask_count + 1;
+        }
+      }
+      // check if that row sets a new record
+      if (mask_count > max_left_right) {
+        max_left_right = mask_count;
+        max_y_index = j;
+      }
+    }
+
+    // now scan once left to right as well
+    print("Scanning mask left to right...")
+    for(let i=0; i<X_STOP; i++) {
+      // look across this column up to down and count
+      let mask_count = 0;
+      for(let j=0; j<Y_STOP; j++) {
+        let mask = maskImg.get(i, j);
+        if (mask[1] > 128) {
+          mask_count = mask_count + 1;
+        }
+      }
+      // check if that row sets a new record
+      if (mask_count > max_up_down) {
+        max_up_down = mask_count;
+        max_x_index = i;
+      }
+    }
+
+    print("Scanning mask done!")
+    if (max_left_right > min_width && max_up_down > min_width) {
+      maskCenter = [max_x_index, max_y_index];
+      maskCenterSize = [max_left_right, max_up_down];
+    }
+}
+
+function maskCenterSearch2(min_width) {
+  // we store the sum of x,y whereever the mask is on
+  // at the end we divide to get the average
+  let mask_x_sum = 0;
+  let mask_y_sum = 0;
+  let mask_count = 0;
+
+  // first scan all rows top to bottom
+  print("Scanning mask top to bottom...")
+  for(let j=0; j<Y_STOP; j++) {
+    // look across this row left to right and count
+    for(let i=0; i<X_STOP; i++) {
+      let mask = maskImg.get(i, j);
+      if (mask[1] > 128) {
+        mask_x_sum = mask_x_sum + i;
+        mask_y_sum = mask_y_sum + j;
+        mask_count = mask_count + 1;
+      }
+    }
+  }
+
+  print("Scanning mask done!")
+  if (mask_count > min_width) {
+    let avg_x_pos = int(mask_x_sum / mask_count);
+    let avg_y_pos = int(mask_y_sum / mask_count);
+    maskCenter = [avg_x_pos, avg_y_pos];
+    print("Center set to: " + maskCenter);
+  }
+}
 
 let renderCounter=0;
 function draw () {
+  angleMode(DEGREES);
   let num_lines_to_draw = 40;
   // get one scanline
-  for(let j=renderCounter; j<renderCounter+num_lines_to_draw && j<1080; j++) {
+  for(let j=renderCounter; j<renderCounter+num_lines_to_draw && j<Y_STOP; j++) {
     for(let i=0; i<X_STOP; i++) {
       colorMode(RGB);
-      let pix = sourceImg.get(i, j);
-      // create a color from the values (always RGB)
-      let col = color(pix);
       let mask = maskImg.get(i, j);
-      let tex = textImg.get(i, j);
+      let tex = texImg.get(int(i-maskCenter[0]/2), int(j-maskCenter[1]/2));
+      let pix = sourceImg.get(i, j);
+      if (mask[1] > 128) {
+        if(tex[1] != 0) {
+          pix[1] = tex[1];
+        }
+      }
 
-      if(mask[0] > 128) {
-        let new_col = [0, 0, 0, 255];
-        new_col[0] = map(50, 0, 100, pix[0], 255);
-        new_col[1] = map(70, 0, 100, pix[1], 255);
-        new_col[2] = map(0, 0, 100, pix[2], 255);
-        set(i, j, new_col);
-      }
-      else {
-        let new_col = [0, 0, 0, 255];
-        new_col[0] = map(40, 0, 100, pix[0], tex[0]);
-        new_col[1] = map(40, 0, 100, pix[1], tex[1]);
-        new_col[2] = map(70, 0, 100, pix[2], tex[2]);
-        // let new_col = color(h, s,  newBrt);
-        set(i, j, new_col);
-      }
+      set(i, j, pix);
     }
   }
   renderCounter = renderCounter + num_lines_to_draw;
   updatePixels();
+
+  if (maskCenter !== null) {
+    // image(texImg, maskCenter[0], maskCenter[1]);
+    strokeWeight(5);
+    fill(0, 255, 0);
+    stroke(255, 0, 0);
+    ellipse(maskCenter[0], maskCenter[1], 100);
+    line(maskCenter[0]-200, maskCenter[1], maskCenter[0]+200, maskCenter[1]);
+    line(maskCenter[0], maskCenter[1]-200, maskCenter[0], maskCenter[1]+200);
+    noFill();
+    // let mcw = maskCenterSize[0];
+    // let mch = maskCenterSize[1];
+    // rect(maskCenter[0]-mcw/2, maskCenter[1]-mch/2, mcw, mch);
+  }
 
   // print(renderCounter);
   if(renderCounter > Y_STOP) {
